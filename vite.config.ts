4b0213d50399f507f3c52294d25eafd921df0c7c
JoaquinFeola <joacoarielfeola@gmail.com>
2025-10-19 20:38:02 -0300

    Refactor project structure and remove unused components
    - Removed the `useSignal` hook implementation as it is no longer needed.
    - Deleted example application files including `App.tsx`, `App.css`, and related styles.
    - Removed the `authStore` for authentication management.
    - Cleaned up the main entry point by removing the `main.tsx` file.
    - Updated `package.json` to reflect changes in dependencies and exports.
    - Modified `vite.config.ts` to configure library build settings and removed unnecessary plugins.
    - Deleted unused type definitions in `vite-env.d.ts`.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from 'vite';
+import path from 'path';
+import { fileURLToPath } from 'node:url'
+import { dirname, resolve } from 'node:path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'dist/main.js'),
+      name: 'signalJS',
+      fileName: "my-lab"
+    },
+    rollupOptions: {
+      external: ["vite"]
+    }
+  },
   resolve: {
     alias: {
       "@": path.join(__dirname, "src")
