88a13be31d68de998ede36a84b46714c3d015722
JoaquinFeola <joacoarielfeola@gmail.com>
2025-10-25 21:00:39 -0300

    something to

--- src/Core/Classes/Signal.class.ts
+++ src/Core/Classes/Signal.class.ts
@@ -1,8 +1,7 @@
 import { ISignal, SignalSubscribeFunction, SignalConfig, } from '../Interfaces/Classes/ISignal';
-import { SignalConfigStorage, SignalStorageTypes } from '../Interfaces/SignalConfigStorage.interface';
+import { SignalConfigStorage } from '../Interfaces/SignalConfigStorage.interface';
 import { SignalStorage } from '../Interfaces/SignalStorage.interface';
 import { LocalStorageAdapter } from '../StorageAdapters/LocalStorage.adapter';
-import { SessionStorageAdapter } from '../StorageAdapters/SessionStorage.adapter';
 import { Observer } from './Observer.class';
 
 export class Signal<T> extends Observer<T> implements ISignal<T> {
@@ -16,8 +15,8 @@ export class Signal<T> extends Observer<T> implements ISignal<T> {
         private config: SignalConfig<T> = {} as SignalConfig<T>
     ) {
         super();
-        if (config.storage) this.initializeStorage();
         this.initializeValue();
+        if (config.storage) this.initializeStorage();
     };
 
 
@@ -38,96 +37,140 @@ export class Signal<T> extends Observer<T> implements ISignal<T> {
         const { storage } = this.config;
 
         if (storage && this.hasStorageValue()) {
-            const stored = this.storage.getValue(storage.name);
-            try {
-                if (stored) {
-                    this.value = JSON.parse(stored);
-                    return;
-                }
-            } catch {
-                console.warn(`[Signal] Invalid JSON in storage: ${storage.name}`);
-            }
-        }
+            const storedValue = this.storage.getValue(storage.name);
+            if (!storedValue) return;
+            const parsedValue: T = JSON.parse(storedValue);
 
-        this.value = this.initialState;
-        // if (storage) this.saveToStorage(this.value);
+            this.value = parsedValue;
+        } else {
+            this.value = this.initialState;
+        }
     }
 
-    private saveToStorage(value: T) {
+    private saveToStorage(value: Partial<T>) {
         if (!this.config.storage) return;
 
         this.storage.setValue(this.config.storage.name, value);
     }
 
-
     private initializeStorage() {
         this.initializeStorageValues();
+
         const { storage } = this.config;
+
         if (!storage) return;
 
+        this.subscribe(value => {
 
-        const storages: Record<SignalStorageTypes, SignalStorage | undefined> = {
-            custom: this.config.storage?.customStorage,
-            localstorage: LocalStorageAdapter,
-            sessionstorage: SessionStorageAdapter
-        };
+            if (typeof value === "object") {
+                if (!this.storageValues) return;
+                const valuesToSave: Record<keyof T, boolean> = Object.fromEntries(
+                    Object.entries(this.storageValues)
+                        .filter(sv => sv[1] == true)
+                ) as Record<keyof T, boolean>;
 
-        const storageSelected = storages[storage.storageType];
+                const partializedValuesToSave: Partial<T> = {};
 
-        if (!storageSelected) {
-            console.warn(`[Signal] Invalid or missing storage adapter for type: ${storage.storageType}`)
-            return;
-        }
-        this.storage = storageSelected;
-
-        this.subscribe((value) => {
-
-            if ( typeof value == "object" && this.storageValues && value ) {
-                const valuesToSave: [keyof T, boolean][] = Object.entries(this.storageValues)
-                                            .filter(value => value[1] == true) as [keyof T, boolean][];
-
-                for (const valueToSave of valuesToSave) {
-                    this.saveToStorage(value[valueToSave[0] as keyof T] as T);
+                for (const valueKey in valuesToSave) {
+                    partializedValuesToSave[valueKey] = value?.[valueKey];
                 }
-            }
-         /*    if (typeof value === "object" && this.storageValues && this.storageValues !== true) {
-                const selectedKeys = Object.entries(this.storageValues)
-                    .filter(([, shouldSave]) => shouldSave)
-                    .map(([key]) => key);
 
-                const filtered = Object.fromEntries(
-                    selectedKeys.map((k) => [k, value?.[k as keyof T]])
-                ) as Partial<T>;
-
-                this.saveToStorage(filtered as T);
-                return;
+                this.saveToStorage(partializedValuesToSave);
             }
 
-            if (this.storageValues === true || typeof value !== "object") {
-                this.saveToStorage(value as T);
-            } */
-        });
+
+
+
+
+
+        })
     }
 
     private initializeStorageValues() {
         const { storage } = this.config;
-        if (!storage || !this.value) return;
-        
-        if ( typeof this.value != "object"  ) {
-            this.storageValues = storage.values ? storage.values : true;
-            return;
-        }
-        
-        const storedValues: Record<keyof T, boolean> = storage.values as Record<keyof T, boolean>; 
+        if (!storage) return;
 
-        for (const key in this.value) {
-             storedValues[key as keyof T] = storedValues[key as keyof T] ?? true;
+        if (typeof storage.values === "object" && typeof this.value === "object") {
+            const storageValues: Record<keyof T, boolean> = storage.values as Record<keyof T, boolean>;
+            const newStoredValues: Record<keyof T, boolean> = {} as Record<keyof T, boolean>;
+
+            for (const valueKey in this.value) {
+                newStoredValues[valueKey] = storageValues[valueKey] ?? true;
+            }
+
+            this.storageValues = newStoredValues as SignalConfigStorage<T>["values"];
         }
-        console.log(storedValues)
-        this.storageValues = storedValues as SignalConfigStorage<T>["values"];
 
     }
-    
+
+    // private initializeStorage() {
+    //     this.initializeStorageValues();
+    //     const { storage } = this.config;
+    //     if (!storage) return;
+
+
+    //     const storages: Record<SignalStorageTypes, SignalStorage | undefined> = {
+    //         custom: this.config.storage?.customStorage,
+    //         localstorage: LocalStorageAdapter,
+    //         sessionstorage: SessionStorageAdapter
+    //     };
+
+    //     const storageSelected = storages[storage.storageType];
+
+    //     if (!storageSelected) {
+    //         console.warn(`[Signal] Invalid or missing storage adapter for type: ${storage.storageType}`)
+    //         return;
+    //     }
+    //     this.storage = storageSelected;
+
+    //     this.subscribe((value) => {
+
+    //         if ( typeof value == "object" && this.storageValues && value ) {
+    //             const valuesToSave: [keyof T, boolean][] = Object.entries(this.storageValues)
+    //                                         .filter(value => value[1] == true) as [keyof T, boolean][];
+
+    //             for (const valueToSave of valuesToSave) {
+    //                 this.saveToStorage(value[valueToSave[0] as keyof T] as T);
+    //             }
+    //         }
+    //      /*    if (typeof value === "object" && this.storageValues && this.storageValues !== true) {
+    //             const selectedKeys = Object.entries(this.storageValues)
+    //                 .filter(([, shouldSave]) => shouldSave)
+    //                 .map(([key]) => key);
+
+    //             const filtered = Object.fromEntries(
+    //                 selectedKeys.map((k) => [k, value?.[k as keyof T]])
+    //             ) as Partial<T>;
+
+    //             this.saveToStorage(filtered as T);
+    //             return;
+    //         }
+
+    //         if (this.storageValues === true || typeof value !== "object") {
+    //             this.saveToStorage(value as T);
+    //         } */
+    //     });
+    // }
+
+    // private initializeStorageValues() {
+    //     const { storage } = this.config;
+    //     if (!storage || !this.value) return;
+
+    //     if ( typeof this.value != "object"  ) {
+    //         this.storageValues = storage.values ? storage.values : true;
+    //         return;
+    //     }
+
+    //     const storedValues: Record<keyof T, boolean> = storage.values as Record<keyof T, boolean>; 
+
+    //     for (const key in this.value) {
+    //          storedValues[key as keyof T] = storedValues[key as keyof T] ?? true;
+    //     }
+    //     console.log(storedValues)
+    //     this.storageValues = storedValues as SignalConfigStorage<T>["values"];
+
+    // }
+
 };
 
 
